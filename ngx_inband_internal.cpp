6337d114dec6fbcc2d9cb1696cc87c3a162c2175
alex b <alex_balk@comcast.com>
2025-02-03 08:24:35 -0800

    add write_styp(), write_emsg(), and supporting functions

--- ngx_inband_internal.cpp
+++ ngx_inband_internal.cpp
@@ -2,6 +2,7 @@
 * Copyright (C) ab 1/29/25
 */
 
+#include <filesystem>
 #include "pugixml.hpp"
 
 #ifdef __cplusplus
@@ -34,7 +35,7 @@ void get_timescale(ngx_http_request_t* r, context_t* ctx) {
     size_t pt_sz;
     ctx->timescale = 0;
 
-    //get the 1) publish time 2) audio timescale
+    //get the 1) publish time 2) audio timescale 3) CURMPD size in bytes
     pugi::xml_document doc;
     if (!doc.load_file(CURMPD))
     {
@@ -70,6 +71,8 @@ void get_timescale(ngx_http_request_t* r, context_t* ctx) {
                       "_INBAND_ could not find audio timescale\n");
         return;
     }
+
+    ctx->mpdsz = std::filesystem::file_size(CURMPD);
 }
 
 void get_tfdt(ngx_http_request_t* r, context_t* ctx) {
@@ -146,6 +149,116 @@ void get_tfdt(ngx_http_request_t* r, context_t* ctx) {
     }
 }
 
+void int2buf32(uint32_t val, uint8_t* valbuf) {
+    valbuf[0] = (uint32_t)val >> 24;
+    valbuf[1] = (uint32_t)val >> 16;
+    valbuf[2] = (uint32_t)val >> 8;
+    valbuf[3] = (uint32_t)val;
+}
+
+void int2buf64(uint64_t val, uint8_t* valbuf) {
+    valbuf[0] = (uint64_t)val >> 56;
+    valbuf[1] = (uint64_t)val >> 48;
+    valbuf[2] = (uint64_t)val >> 40;
+    valbuf[3] = (uint64_t)val >> 32;
+    valbuf[4] = (uint64_t)val >> 24;
+    valbuf[5] = (uint64_t)val >> 16;
+    valbuf[6] = (uint64_t)val >> 8;
+    valbuf[7] = (uint64_t)val;
+}
+
+void write_styp(FILE* fp) {
+    uint8_t  styp_sz_buf[4];
+    uint32_t zero = 0x00;
+    uint8_t  styp[4] = {'s', 't', 'y', 'p'};
+    uint8_t  mp41[4] = {'m', 'p', '4', '1'};
+
+    uint8_t  seg_buf[20];
+    uint32_t styp_sz = 12; //4*3
+
+    int2buf32(styp_sz, styp_sz_buf);
+
+    memcpy(seg_buf, styp_sz_buf, 4);
+    memcpy(&seg_buf[4], styp, 4);
+    memcpy(&seg_buf[8], mp41, 4);
+    memcpy(&seg_buf[12], &zero, 4);
+    memcpy(&seg_buf[16], mp41, 4);
+
+    fwrite(seg_buf, 1, 20, fp);
+    fflush(fp);
+}
+
+void write_emsg(ngx_http_request_t* r, FILE* fp, context_t* ctx) {
+    uint32_t emsg_sz;
+    uint8_t  emsg_sz_buf[4];
+    uint8_t  emsg[4] = {'e', 'm', 's', 'g'};
+    uint32_t pubtime_sz;
+    uint32_t siu_sz;
+    uint8_t  version = 0x01;
+    uint8_t  flags[3] = {0x00};
+    uint8_t  timescale_buf[4];
+    uint8_t  tfdt_buf[8];
+    uint8_t  event_dur[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+    uint32_t id = 0xDEADBEEF;
+    uint16_t value = 0x3300;
+    size_t   num;
+
+    const char* scheme_id_uri = (const char*)"urn:mpeg:dash:event:2012";
+
+    pubtime_sz = strlen(ctx->pubtime);
+    siu_sz = strlen(scheme_id_uri);
+    emsg_sz = 24 +             //len of constant fields
+              siu_sz + 1 +     //+1 for '\0'
+              2 +              //len of "value" field
+              pubtime_sz + 1 + //+1 for '\0'
+              ctx->mpdsz;
+    printf(" emsg sz : %u\n\n", emsg_sz);
+
+    //+8 because 'emsg_sz_buf' and 'emsg' are not included in emsg_sz
+    uint8_t* seg_buf = (uint8_t*)calloc(emsg_sz + 8, 1);
+    if (seg_buf == NULL)
+    {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "_INBAND_ write_emsg, calloc returned NULL\n");
+        return;
+    }
+
+    int2buf32(emsg_sz, emsg_sz_buf);
+    memcpy(seg_buf, emsg_sz_buf, 4);
+    memcpy(&seg_buf[4], emsg, 4);
+    memcpy(&seg_buf[8], &version, 1);
+    memcpy(&seg_buf[9], flags, 3);
+
+    int2buf32(ctx->timescale, timescale_buf);
+    memcpy(&seg_buf[12], timescale_buf, 4);
+
+    int2buf64(ctx->tfdt, tfdt_buf);
+    memcpy(&seg_buf[16], tfdt_buf, 8);
+    memcpy(&seg_buf[24], event_dur, 4);
+    memcpy(&seg_buf[28], &id, 4);
+    memcpy(&seg_buf[32], scheme_id_uri, 25); //25 = strlen(scheme_id_uri)+1
+    memcpy(&seg_buf[57], &value, 2);
+    memcpy(&seg_buf[59], ctx->pubtime, pubtime_sz+1);
+
+    //read in the mpd to seg_buf[59+pubtime_sz+1]
+    FILE *fmpd = fopen(CURMPD, "rb");
+    num = fread(&seg_buf[59+pubtime_sz+1], 1, ctx->mpdsz, fmpd);
+    if (num < ctx->mpdsz)
+    {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "_INBAND_ write_emsg num less than mpdsz. num: %d mpdsz: %d\n",
+                      num, ctx->mpdsz);
+        return;
+    }
+    fclose(fmpd);
+
+    //write to file
+    fwrite(seg_buf, emsg_sz + 8, 1, fp);
+    fflush(fp);
+    free(seg_buf);
+    seg_buf = NULL;
+}
+
 void process_audio(ngx_http_request_t* r) {
     context_t ctx;
 
@@ -162,6 +275,21 @@ void process_audio(ngx_http_request_t* r) {
     printf(" pub time: %s\n", ctx.pubtime);
     printf("     tfdt: %lu\n", ctx.tfdt);
     printf("  version: %u\n", ctx.version);
+
+    FILE* fp;
+    fp = fopen(ctx.audio_seg_name, "wb");
+    if (fp == NULL)
+    {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "_INBAND_ could not open audio seg file \"%s\" for writing\n", ctx.audio_seg_name);
+        return;
+    }
+
+    write_styp(fp);
+    write_emsg(r, fp, &ctx);
+    //concat_audio_seg(fp, ctx);
+    fclose(fp);
+
 
 #if 0
     /*
